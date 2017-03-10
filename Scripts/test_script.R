0e1989bdc0375f6df181c8a401b56d8a70eb3dd8
Kelly Croteau <kellyc@windwardenv.com>
2017-03-10 14:11:21 -0500

    Everything is Changed
    I can see clearly now.

---
+++ Scripts/test_script.R
@@ -0,0 +1,14 @@
+#Initial script
+
+
+# These are some changes to the script
+x = 1
+y = 2
+z = x * y
+
+# Hello.  Me again. Still figuring things out.
+
+# making a plot
+pdf("Figures/plot.pdf")
+plot(1:5,1:5,xlab="x",ylab="y")
+dev.off()
--- test_script.R
+++
@@ -1,9 +0,0 @@
-#Initial script
-
-
-# These are some changes to the script
-x = 1
-y = 2
-z = x * y
-
-# Hello.  Me again. Still figuring things out.
